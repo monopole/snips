eb7500a2f8f384c3db27edf2e395257e5797fafd
Jeffrey Regan <jregan@google.com>
2019-12-17 14:23:45 -0800

    command args

--- main.go
+++ main.go
@@ -5,111 +5,141 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/google/go-github/v28/github"
 	"golang.org/x/oauth2"
 )
 
-
-const  (
-	org1  = "kubernetes-sigs"
-	org2  = "monopole"
-	repo1 = "kustomize"
-	repo2 = "mdrip"
-)
-// reviews  query:
-//  -author:monopole commenter:monopole updated:2019-11-25..2019-12-01
-//
-// PR query:
-//  author:monopole is:pr merged:2019-11-25..2019-12-01
+type questioner struct {
+	ghOrg     string
+	ghRepo    string
+	user      string
+	dateStart time.Time
+	dateEnd   time.Time
+	client    *github.Client
+	ctx       context.Context
+}
 
 func main() {
-	fmt.Println("hey")
-	token := os.Getenv("GITHUB_AUTH_TOKEN")
-	if token == "" {
-		 log.Fatal("Unauthorized: No token present")
+	if len(os.Args) < 7 {
+		fmt.Print(`usage:
+  snips {githubOrg} {githubRepo} {user} {dateStart} {dateEnd} {githubAuthToken}
+e.g.
+  snips kubernetes-sigs kustomize monopole 2019-11-25 2019-12-01 deadbeef0000deadbeef
+`)
+		os.Exit(1)
 	}
-
 	ctx := context.Background()
+	q := questioner{
+		ghOrg:     os.Args[1],
+		ghRepo:    os.Args[2],
+		user:      os.Args[3],
+		dateStart: parseDate(os.Args[4]),
+		dateEnd:   parseDate(os.Args[5]),
+		ctx:       ctx,
+		client:    makeClient(ctx, os.Args[6]),
+	}
+	//q.reportOrgs(client)
+	//q.reportUser(client, ctx)
+	q.reportReviews()
+	q.reportPrs()
+}
+
+func parseDate(arg string) time.Time {
+	t, err := time.Parse("2006-01-02", arg)
+	if err != nil {
+		fmt.Printf("Trouble with date specification: '%s'\n", arg)
+		log.Fatal(err)
+	}
+	return t
+}
+
+func makeClient(ctx context.Context, token string) *github.Client {
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
 	)
 	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
-	//reportOrgs(client)
-	//reportUser(client, ctx)
-	reportReviews(client, ctx)
-	reportPrs(client, ctx)
+	return github.NewClient(tc)
 }
 
-func reportReviews(client *github.Client, ctx context.Context) {
-
-		opts := &github.SearchOptions{
-			Sort:        "",
-			Order:       "",
-			TextMatch:   false,
+// The query excludes the user as the author, looking
+// for the user only in comments.
+func (q *questioner) reportReviews() {
+	results, _, err := q.client.Search.Issues(
+		q.ctx,
+		fmt.Sprintf(
+			"-author:%s commenter:%s updated:%s..%s",
+			q.user, q.user,
+			q.dateStart.Format("2006-01-02"),
+			q.dateEnd.Format("2006-01-02")),
+		&github.SearchOptions{
+			Sort:      "",
+			Order:     "",
+			TextMatch: false,
 			ListOptions: github.ListOptions{
 				Page:    0,
 				PerPage: 100,
 			},
-		}
-	results, _, err := client.Search.Issues(
-		ctx,
-		"-author:monopole commenter:monopole updated:2019-12-09..2019-12-15",
-		opts)
+		})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("## Reviews")
 	for _, r := range results.Issues {
-		fmt.Printf(
-			" - [%s](https://github.com/kubernetes-sigs/kustomize/pull/%d) \n",
-			r.GetTitle(), r.GetNumber())
+		q.printPrLink(r.GetTitle(), r.GetNumber(), r.GetUpdatedAt())
 	}
 }
-func reportPrs(client *github.Client, ctx context.Context) {
-	opts := &github.PullRequestListOptions{
-		State:       "closed",
-		Head:        "",
-		Base:        "",
-		Sort:        "",
-		Direction:   "desc",
-		ListOptions: github.ListOptions{
-			Page:    0,
-			PerPage: 100,
-		},
-	}
-	results, _, err := client.PullRequests.List(ctx, org1, repo1, opts)
+
+// Alternative query:
+//  author:monopole is:pr merged:2019-11-25..2019-12-01
+func (q *questioner) reportPrs() {
+	results, _, err := q.client.PullRequests.List(
+		q.ctx,
+		q.ghOrg,
+		q.ghRepo,
+		&github.PullRequestListOptions{
+			State:     "closed",
+			Head:      "",
+			Base:      "",
+			Sort:      "",
+			Direction: "desc",
+			ListOptions: github.ListOptions{
+				Page:    0,
+				PerPage: 100,
+			},
+		})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("## PRS")
 	for _, r := range results {
-		if r.GetUser().GetLogin() == "monopole" {
-			t := r.GetMergedAt().Format("2006-01-02")
-			fmt.Printf(
-				" - [%s](https://github.com/kubernetes-sigs/kustomize/pull/%d) <!-- %s -->\n",
-				r.GetTitle(), r.GetNumber(), t)
+		if r.GetUser().GetLogin() == q.user /* TODO add date check */ {
+			q.printPrLink(
+				r.GetTitle(), r.GetNumber(), r.GetMergedAt())
 		}
 	}
 }
 
-func reportOrgs(client *github.Client) {
-	orgs, _, err := client.Organizations.List(
-		context.Background(), org2, nil)
+func (q *questioner) printPrLink(title string, prNum int, date time.Time) {
+	fmt.Printf(
+		" - %s [%s](https://github.com/%s/%s/pull/%d)\n",
+		date.Format("2006-01-02"), title, q.ghOrg, q.ghRepo, prNum)
+}
+
+func (q *questioner) reportOrgs() {
+	orgs, _, err := q.client.Organizations.List(
+		q.ctx, q.ghOrg, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	for i, organization := range orgs {
 		fmt.Printf("%v. %v\n", i+1, organization.GetLogin())
 	}
 }
 
-
-func reportUser(client *github.Client, ctx context.Context) {
-	user, _, err := client.Users.Get(ctx, "monopole")
+func (q *questioner) reportUser() {
+	user, _, err := q.client.Users.Get(q.ctx, q.user)
 	if err != nil {
 		log.Fatal(err)
 	}
